199f83cfdda23aa887e2ef04affc55aa468e8c4d
tangs <tangsong1988kaka@gmail.com>
2019-02-18 23:06:37 +0800

    update.

--- src/4.js
+++ src/4.js
@@ -64,3 +64,11 @@ line 2.`)
     const tmp = addrs => `${addrs.map(addr => `${addr.x * addr.y}`)}`;
     console.log(tmp([{x: 1, y: 2}, {x: 3, y: 4}, {x: 5, y: 6}]));
 }
+
+{
+    let template = `
+    <ul>
+    
+    </ul>
+    `;
+}
