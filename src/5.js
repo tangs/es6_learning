2bb301eb5334523ae4bdf9fbf98e7d06891f4195
tangs <tangsong1988kaka@gmail.com>
2019-02-19 17:14:07 +0800

    update.

--- src/5.js
+++ src/5.js
@@ -23,3 +23,58 @@
     console.log(s.length);
     console.log(getLength(s));
 }
+
+{
+    const s = 'aaa_aa_a_aaaa';
+    const s1 = 'bb_a_b_aaaa';
+    const r1 = /a+/g;
+    const r2 = /a+_?/y;
+    do {
+        let ret = r1.exec(s);
+        console.log(ret);
+        if (!ret) {
+            break;
+        }
+    } while(1);
+    r1.lastIndex = 0;
+    console.log(r1.exec(s));
+    console.log(s);
+    do {
+        let ret = r2.exec(s);
+        console.log(ret, r2.lastIndex);
+        if (!ret) {
+            break;
+        }
+    } while(1);
+}
+
+{
+    const s = 'a:1 b:3 c:6 d:50';
+    const exp = /(?<k>\w+):(?<v>\w+)/g;
+    const obj = new Object();
+    do {
+        const ret = exp.exec(s);
+        // console.log(ret, exp.lastIndex);
+        if (!ret) {
+            break;
+        } else {
+            // const g = ret.groups;
+            const {groups: {k, v}} = ret;
+            obj[k] = v;
+            console.log(k, v);
+        }
+    } while(1);
+    console.log(obj);
+}
+
+{
+    const s = '2019/08/03';
+    const exp = /(?<y>\d{4})\D*(?<m>\d{1,2})\D*(?<d>\d{1,2})/;
+    const s1 = s.replace(exp, '$<y>-$<m>-$<d>');
+    const s2 = s.replace(exp, (matched, c1, c2, c3, pos, src, groups) => {
+        const {y, m , d} = groups;
+        return `${y}\\${m}\\${d}`;
+    });
+    console.log(s1);
+    console.log(s2);
+}
