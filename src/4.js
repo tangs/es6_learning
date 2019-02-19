81e532cbd11c91560c03939984d55d1d02d1d933
tangs <tangsong1988kaka@gmail.com>
2019-02-19 11:25:02 +0800

    learning compile.

--- src/4.js
+++ src/4.js
@@ -66,9 +66,37 @@ line 2.`)
 }
 
 {
+
+    function compile(template) {
+        const evalExpr = /<%=(.+?)%>/g;
+        const expr = /<%([\s\S]+?)%>/g;
+        template = template
+        .replace(evalExpr, '`); \n echo($1); \n echo(`')
+        .replace(expr, '`); \n $1 \n echo(`');
+        template = `echo(\`${template}\`)`;
+        const script = 
+        `(function parse(data) {
+            let output = '';
+
+            function echo(html) {
+                output += html;
+            }
+
+            ${template}
+
+            return output;
+        })`;
+        return script;
+    }
     let template = `
     <ul>
-    
+        <% for(let i = 0; i < data.supplies.length; ++i) { %>
+            <li><%= data.supplies[i] %></li>
+        <% } %>
     </ul>
     `;
+    const script = compile(template);
+    const parse = eval(script);
+    const txt = parse({supplies: ['tangs', 'kaka', 'Rivaldo', 'Ronaldo']});
+    console.log(txt);
 }
