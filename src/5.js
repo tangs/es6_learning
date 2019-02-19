74a2e65fd6f1ca42031e99d782f1f94e05d230dc
tangs <tangsong1988kaka@gmail.com>
2019-02-19 16:23:34 +0800

    update.

--- src/5.js
+++ src/5.js
@@ -3,10 +3,23 @@
     const txt = 'aBcgAbcabc';
     console.log(txt.match(regex));
     console.log(regex.test(txt));
+    console.log(regex.flags);
+    console.log(regex.unicode);
 }
 
 {
     const s = '𠮷';
     console.log(/^.$/.test(s));
     console.log(/^.$/u.test(s));
+}
+
+{
+    function getLength(txt) {
+        const result = txt.match(/./gu);
+        return result ? result.length: 0;
+    }
+
+    const s = '𠮷𠮷';
+    console.log(s.length);
+    console.log(getLength(s));
 }
