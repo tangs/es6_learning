e5378be60129674863f8ee551ef9ae9f966aac04
tangs <tangsong1988kaka@gmail.com>
2019-02-20 16:14:51 +0800

    update.

--- src/8.js
+++ src/8.js
@@ -5,3 +5,21 @@
     const arr = [1, 2, 3];
     func(...arr);
 }
+{
+    const old = Number.prototype[Symbol.iterator];
+    Number.prototype[Symbol.iterator] = function*() {
+        let num = this.valueOf();
+        do {
+            yield num % 10;
+            num = Math.trunc(num / 10);
+        } while(num > 0);
+    }
+    console.log([...1234560]);
+    for (let n of [...9876543210]) {
+        console.log(n);
+    }
+    for (let ch of 'abcdefg') {
+        console.log(ch);
+    }
+    Number.prototype[Symbol.iterator] = old;
+}
