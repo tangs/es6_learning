170e55376f67c8f1ccf9f02e7704e5f6dab95eb8
tangs <tangsong1988kaka@gmail.com>
2019-02-21 16:59:40 +0800

    update.

--- src/12.js
+++ src/12.js
@@ -8,3 +8,22 @@
 {
     console.log([...new Set('abcddddbc')].join(''));
 }
+
+{
+    const arr = [1, 2, 3, 4, 5, 6, 7].filter((v) => (v & 1) === 0);
+    console.log(arr);
+}
+
+{
+    const k = {k: 'v'};
+    const k1 = {k: 'v1'};
+    const m = new Map();
+    m.set(k, 'v');
+    m.set(k1, 'v1');
+    const o = {
+        [k]: 'vv',
+        [k1]: 'vv1'
+    };
+    console.log(m);
+    console.log(o);
+}
