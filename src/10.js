a08fa25df014a831e5ffb2d2bbcaf8de74e6f77a
tangs <tangsong1988kaka@gmail.com>
2019-02-21 16:05:50 +0800

    update.

--- src/10.js
+++ src/10.js
@@ -71,3 +71,51 @@
     }
     console.log('a,bdd,ch3,d,egd'.split(new MySplitter(',')));
 }
+
+{
+    class MyIterator {
+        constructor(...paras) {
+            this.value = paras;
+        }
+        *[Symbol.iterator]() {
+            const value = this.value;
+            if (value instanceof Array) {
+                for (let i = value.length - 1; i >= 0; --i) {
+                    yield value[i];
+                }
+            } else {
+                yield value;
+            }
+        }
+    }
+    const iter = new MyIterator(1, 2, 3, 4, 5);
+    console.log(...iter);
+}
+
+{
+    class MyClass {
+        constructor(value) {
+            this.value = value;
+        }
+        [Symbol.toPrimitive](hint) {
+            switch (hint) {
+                case 'number':
+                    return 100 + this.value;
+                case 'string':
+                    return `str:${this.value}`;
+                case 'default':
+                    return `def:${this.value}`;
+                default:
+                    throw new Error(`error hint type:${hint}`);
+            }
+        }
+    }
+
+    const my = new MyClass(111);
+    console.log(2 * my);
+    console.log(2 + my);
+    console.log('def:111' == my);
+    console.log('111' == my);
+    console.log('' + my);
+    console.log('aaa' + my);
+}
