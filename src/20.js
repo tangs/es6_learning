f7532501f5592f29e75dbf8794cad2b3b92bf7d5
tangs <tangsong1988kaka@gmail.com>
2019-02-27 18:09:32 +0800

    update.

--- src/20.js
+++ src/20.js
@@ -1,11 +1,15 @@
 {
     class Point {
-
+        constructor(x, y) {
+            [this.x, this.y] = [x, y];
+        }
     }
 
     console.log(typeof Point);
     console.log(Point === Point.prototype.constructor);
 
-    const p = new Point();
+    const p = new Point(1, 2);
+    const p1 = new p.constructor(3, 4);
+    console.log(p1);
     console.log(Point === p.constructor);
 }
