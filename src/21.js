c9354bc92514e63486a6e75e409519d71b582cbe
tangs <tangsong1988kaka@gmail.com>
2019-03-05 23:27:38 +0800

    update.

--- src/21.js
+++ src/21.js
@@ -4,3 +4,41 @@
     const c = [...a, ...b];
     console.log(c);
 }
+
+{
+    function A(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    // A.prototype.a = function() {
+    //     console.log('call A.a().');
+    // };
+    A.prototype = {
+        constructor: A,
+        a: function() {
+            console.log('call A.prototype.a().');
+        }
+    };
+    A.c = function() {
+        console.log('call A.c().');
+    };
+
+    function B(...pars) {
+        // super(x, y);
+        const [, , z] = pars;
+        A.apply(this, pars);
+        this.z = z;
+    }
+    // B.prototype = new A();
+    B.prototype = Object.create(A.prototype);
+    B.prototype.constructor = A;
+
+    B.prototype.b = function() {
+        console.log(`x:${this.x}, y:${this.y}, z:${this.z}`);
+    }
+
+    const a = new B(1, 2, 3);
+    a.b();
+    a.a();
+    // a.c();
+}
