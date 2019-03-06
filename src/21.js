8a760d372f2ccac2ccf0cf4802f585c5f39a468e
tangs <tangsong1988kaka@gmail.com>
2019-03-06 10:33:52 +0800

    [update]

--- src/21.js
+++ src/21.js
@@ -3,4 +3,11 @@
     const b = [4, 5, 6];
     const c = [...a, ...b];
     console.log(c);
+
+    const map = new Map();
+    map.set('1', 1);
+    map.set('2', 2);
+    for (const [k, v] of map.entries()) {
+        console.log(k, v);
+    }
 }
